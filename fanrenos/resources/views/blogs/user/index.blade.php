0974648ed999ce853199024204e032504b5fcb42
Mostanily <p229885381@163.com>
2017-10-15 13:41:40 +0800

    忘了个人中心还有评论内容展示，需要用到表情，现补上。

--- fanrenos/resources/views/blogs/user/index.blade.php
+++ fanrenos/resources/views/blogs/user/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.base')
 @section('css')
-
+<link rel="stylesheet" type="text/css" href="{{asset('css/sinaFaceAndEffec.css')}}" />
+<style type="text/css">
+    .face-icon{margin-right: 8px;cursor: pointer;}
+</style>
 @stop
 @section('content')
     @include('blogs.user.particals.info')
@@ -17,7 +20,15 @@
         </div>
     </div>
 @stop
-
 @section('js')
-
+<script type="text/javascript" src="{{asset('js/main.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/sinaFaceAndEffec.js')}}"></script>
+<script type="text/javascript">
+    window.onload = function(){
+        $('li.list-group-item').each(function(){
+            var inputText = $(this).find('.comment_content').html();
+            $(this).find('.comment_content').html(AnalyticEmotion(inputText));
+        });
+    }
+</script>
 @stop
--- fanrenos/resources/views/blogs/user/particals/comments.blade.php
+++ fanrenos/resources/views/blogs/user/particals/comments.blade.php
@@ -1,18 +1,17 @@
 <ul class="am-comments-list am-comments-list-flip">
     @forelse($comments as $comment)
         <li class="list-group-item">
-
-                @if($comment->commentable_type == 'articles')
-                    <a href="{{ url('blog/'.$comment->article->slug) }}" target="_blank">{{ $comment->article->title }}</a>
-                @else
-                    <a href="{{ url('discussion', ['id' => $comment->commentable_id]) }}" target="_blank">{{ $comment->article->title }}</a>
-                @endif
-
-                <span class="meta">
-                    in <span class="timeago">{{ $comment->created_at->diffForHumans() }}</span>
-                </span>
-
+            @if($comment->commentable_type == 'articles')
+                <a href="{{ url('blog/'.$comment->article->slug) }}" target="_blank">{{ $comment->article->title }}</a>
+            @else
+                <a href="{{ url('discussion', ['id' => $comment->commentable_id]) }}" target="_blank">{{ $comment->article->title }}</a>
+            @endif
+            <span class="meta">
+                in <span class="timeago">{{ $comment->created_at->diffForHumans() }}</span>
+            </span>
+            <div class="comment_content">
                 <p>{!! json_decode($comment->content)->html !!}</p>
+            </div>
         </li>
     @empty
         <li class="nothing">{{ lang('Nothing') }}</li>
